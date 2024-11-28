2d31e1f6396c0f9674e9d7c125c9fcb0b5a69573
Abdoulaye Diaw <abdoulaye.diaw@joza-it.fr>
2024-11-28 07:54:52 +0100

    all working butttt

--- src/main/java/com/example/springiapromptdemo/repositories/DataSetRepository.java
+++ src/main/java/com/example/springiapromptdemo/repositories/DataSetRepository.java
@@ -1,7 +1,8 @@
 package com.example.springiapromptdemo.repositories;
 
-import com.example.springiapromptdemo.datasets.GraphDatasetElement;
+import com.example.springiapromptdemo.entities.DataSet;
+import com.example.springiapromptdemo.entities.GraphDatasetElement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DataSetRepository extends JpaRepository<GraphDatasetElement,Long> {
+public interface DataSetRepository extends JpaRepository<DataSet,Long> {
 }
--- src/main/java/com/example/springiapromptdemo/services/OllamaService.java
+++ src/main/java/com/example/springiapromptdemo/services/OllamaService.java
@@ -1,5 +1,6 @@
 package com.example.springiapromptdemo.services;
 
+import com.example.springiapromptdemo.entities.LLMPrompt;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
@@ -14,12 +15,16 @@ public class OllamaService {
 
     @Autowired
     OllamaChatModel ollamaChatModel;
-    public String callOllama(){
 
+    @Autowired PromptService promptService;
+
+    @Autowired DataSetService dataSetService;
+
+    public String callOllama(){
         SystemPromptTemplate systemPromptTemplate= new SystemPromptTemplate(
                 """
-                        I need you to give me the best movie on the given category: {category}
-                        on the given year : {year}.
+                        I need you to give me the best movie on the given category: action
+                        on the given year : 2020.
                         the outpout shourld be in json format including the following fields :
                          - category <The given category>
                          - year <The year of the movie>
@@ -30,7 +35,36 @@ public class OllamaService {
                         """
         );
 
+
+
         Prompt prompt= systemPromptTemplate.create(Map.of("category","action","year",2020));
+        ChatResponse chatResponse = ollamaChatModel.call(prompt);
+        System.out.println("chatResponse = " + chatResponse);
+        return chatResponse.getResult().getOutput().getContent();
+    }
+
+
+    public String callOllama(Long promptId){
+        LLMPrompt savedPrompts = promptService.getOnePrompt(promptId);
+        Prompt prompt= new Prompt(savedPrompts.getSystemPrompt());
+        ChatResponse chatResponse = ollamaChatModel.call(prompt);
+        System.out.println("chatResponse = " + chatResponse);
+        return chatResponse.getResult().getOutput().getContent();
+    }
+
+
+    public String callOllama(Long promptId,Long datasetId){
+        LLMPrompt savedPrompts = promptService.getOnePrompt(promptId);
+        StringBuffer contextText=new StringBuffer();
+        contextText.append("Tu effectuera la tache demandé en tenant compte uniquement des informations fournis ci-dessous:");
+        dataSetService.getDatasetElements(datasetId).forEach(graphDatasetElement -> {
+            contextText.append(graphDatasetElement.toString());
+        });
+
+       String finalSystemPrompt=savedPrompts.getSystemPrompt().concat(contextText.toString());
+
+
+        Prompt prompt= new Prompt(finalSystemPrompt);
         ChatResponse chatResponse = ollamaChatModel.call(prompt);
         System.out.println("chatResponse = " + chatResponse);
         return chatResponse.getResult().getOutput().getContent();
